8ae19c4957c00e5e587465825f017bac7223a4c0
MuhammadLateef <ml129kmg@gmail.com>
2024-03-13 19:44:15 +0500

    config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    output: "export",
-    reactStrictMode: false,
-    trailingSlash: true,
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
+  eslint: {
+   ignoreDuringBuilds: true,
+ },
+}
+
+module.exports = nextConfig
